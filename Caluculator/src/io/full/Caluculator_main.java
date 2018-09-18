8755d3cfd90b69ef23c56a100854ced7d4f944d7
Srinivasan <srini0593@gmail.com>
2018-09-18 17:38:13 +0530

    v3

--- Caluculator/src/io/full/Caluculator_main.java
+++ Caluculator/src/io/full/Caluculator_main.java
@@ -4,7 +4,7 @@ public class Caluculator_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This from srini");
+		System.out.println("This from master branch");
 		System.out.println("This from git");
 		
 	}
