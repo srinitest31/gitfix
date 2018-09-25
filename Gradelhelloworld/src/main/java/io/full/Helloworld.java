ead99362d6a8ca884a64fe00615443a9a464c6ed
Srinivasan <srini0593@gmail.com>
2018-09-25 15:49:19 +0530

    v1

---
+++ Gradelhelloworld/src/main/java/io/full/Helloworld.java
@@ -0,0 +1,16 @@
+package io.full;
+class A{
+	String print() {
+		return "srini";
+	}
+}
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A ob=new A();
+		System.out.println(ob.print());
+	}
+
+}
---
+++ Gradle_webapplication/src/main/java/io/full/Helloworld.java
@@ -0,0 +1,8 @@
+package io.full;
+
+public class Helloworld {
+   
+	public static void main(String[] args) {
+		System.out.println("this is main");
+	}
+}
